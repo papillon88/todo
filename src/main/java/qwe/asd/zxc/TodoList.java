8038793f17ee68ed3a3f8ea6b05e4546a0361136
papillon <devopreet.ch@gmail.com>
2017-07-17 15:22:01 -0500

    basic todo layout

---
+++ src/main/java/qwe/asd/zxc/TodoList.java
@@ -0,0 +1,11 @@
+package qwe.asd.zxc;
+
+import com.vaadin.ui.VerticalLayout;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by papillon on 7/17/2017.
+ */
+@Component
+public class TodoList extends VerticalLayout{
+}
--- src/main/java/qwe/asd/zxc/TodoUI.java
+++ src/main/java/qwe/asd/zxc/TodoUI.java
@@ -1,17 +1,56 @@
 package qwe.asd.zxc;
 
+import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by papillon on 7/17/2017.
  */
 @SpringUI
+@Theme("valo")
 public class TodoUI extends UI {
+
+    private VerticalLayout layout;
+    @Autowired
+    private TodoList todoList;
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label("I'm the root UI!"));
+        //setContent(new Label("I'm the root UI!"));
+        setupLayout();
+        addHeader();
+        addForm();
+        addTodoList();
+        addActionButton();
+    }
+
+    private void addActionButton() {
+        Button delete = new Button("Delete completed");
+        layout.addComponent(delete);
+    }
+
+    private void addTodoList() {
+        layout.addComponent(todoList);
+    }
+
+    private void addForm() {
+        HorizontalLayout formLayout = new HorizontalLayout();
+        TextField task = new TextField();
+        Button add = new Button("Add");
+        formLayout.addComponents(task,add);
+        layout.addComponent(formLayout);
+    }
+
+    private void addHeader() {
+        Label header = new Label("TODO");
+        layout.addComponent(header);
+    }
+
+    private void setupLayout() {
+        layout = new VerticalLayout();
+        setContent(layout);
     }
 }
